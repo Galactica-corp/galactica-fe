592fe35cc547dfbf9c4767469f2f3b485f9f4db6
Aleksandr Abanin <aa@occam.fi>
2023-04-19 08:36:19 +0300

    remove layout

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,23 @@
+const FS_LAYERS = [
+  "app",
+  "processes",
+  "pages",
+  "widgets",
+  "features",
+  "entities",
+  "shared",
+];
+
+const FS_SEGMENTS = ["ui", "hooks", "utils", "config", "assets"];
+
+const getUpperLayers = (layer) => FS_LAYERS.slice(0, FS_LAYERS.indexOf(layer));
+
+const FS_SLICED_LAYERS_REG = getUpperLayers("shared").join("|");
+const FS_SEGMENTS_REG = [
+  ...FS_SEGMENTS,
+  ...FS_SEGMENTS.map((seg) => `${seg}.*`),
+].join("|");
+
 /** @type {import("eslint").ESLint.ConfigData} */
 module.exports = {
   env: {
@@ -110,6 +130,50 @@ module.exports = {
             disallow: ["app"],
             message: "Pages must not import more complex 'business' logic",
           },
+        ],
+      },
+    ],
+    "import/no-internal-modules": [
+      "error",
+      {
+        allow: [
+          /**
+           * Allow not segments import from slices
+           * @example
+           * 'entities/form/ui' // Fail
+           * 'entities/form' // Pass
+           */
+          `**/*(${FS_SLICED_LAYERS_REG})/!(${FS_SEGMENTS_REG})`,
+
+          /**
+           * Allow slices with structure grouping
+           * @example
+           * 'features/auth/form' // Pass
+           */
+          `**/*(${FS_SLICED_LAYERS_REG})/!(${FS_SEGMENTS_REG})/!(${FS_SEGMENTS_REG})`,
+
+          /**
+           * Allow not segments import in shared segments
+           * @example
+           * 'shared/ui/button' // Pass
+           */
+          `**/*shared/*(${FS_SEGMENTS_REG})/!(${FS_SEGMENTS_REG})`,
+
+          /**
+           * Allow import from segments in shared
+           * @example
+           * 'shared/ui' // Pass
+           */
+          `**/*shared/*(${FS_SEGMENTS_REG})`,
+
+          /** allow global modules */
+          `**/node_modules/**`,
+
+          /**
+           * allow custom shared segments with _prefix
+           */
+          `**/*shared/_*`,
+          `**/*shared/_*/*`,
         ],
       },
     ],
