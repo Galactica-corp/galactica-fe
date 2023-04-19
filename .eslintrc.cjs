72ca9db0fbb2ff8cc09c94720d3e0981a975302b
Aleksandr Abanin <aa@occam.fi>
2023-04-19 08:57:09 +0300

    removed public api rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -133,49 +133,5 @@ module.exports = {
         ],
       },
     ],
-    "import/no-internal-modules": [
-      "error",
-      {
-        allow: [
-          /**
-           * Allow not segments import from slices
-           * @example
-           * 'entities/form/ui' // Fail
-           * 'entities/form' // Pass
-           */
-          `**/*(${FS_SLICED_LAYERS_REG})/!(${FS_SEGMENTS_REG})`,
-
-          /**
-           * Allow slices with structure grouping
-           * @example
-           * 'features/auth/form' // Pass
-           */
-          `**/*(${FS_SLICED_LAYERS_REG})/!(${FS_SEGMENTS_REG})/!(${FS_SEGMENTS_REG})`,
-
-          /**
-           * Allow not segments import in shared segments
-           * @example
-           * 'shared/ui/button' // Pass
-           */
-          `**/*shared/*(${FS_SEGMENTS_REG})/!(${FS_SEGMENTS_REG})`,
-
-          /**
-           * Allow import from segments in shared
-           * @example
-           * 'shared/ui' // Pass
-           */
-          `**/*shared/*(${FS_SEGMENTS_REG})`,
-
-          /** allow global modules */
-          `**/node_modules/**`,
-
-          /**
-           * allow custom shared segments with _prefix
-           */
-          `**/*shared/_*`,
-          `**/*shared/_*/*`,
-        ],
-      },
-    ],
   },
 };
