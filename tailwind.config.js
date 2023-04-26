211ae8befc182a3aa40763bd1872921a4a8130d4
HerbertLiskin <gkljatsko@gmail.com>
2023-04-26 11:06:51 +0200

    add new colors and screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      ms: '320px',
+      mm: '410px',
+      ml: '600px',
+      t: '768px',
+      dxs: '1024px',
+      ds: '1240px',
+      dm: '1440px',
+      dl: '1600px',
+      dxl: '1900px',
+    },
     colors: {
       // Use this service to name color
       // https://chir.ag/projects/name-that-color
@@ -9,6 +20,8 @@ export default {
       mineShaft: "#2B2B2B",
       sandyBrown: "#F49756",
       grayNickel: "#C9C9C7",
+      salmon: '#FF8A71',
+      scarlet: '#F31C0E',
       transparent: "transparent",
     },
     fontFamily: {
