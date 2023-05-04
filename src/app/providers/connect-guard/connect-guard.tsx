aa0be9dab4ae75742b8f8307228bddaf280bae48
Aleksandr Abanin <aa@occam.fi>
2023-05-04 13:18:30 +0300

    check flask

--- src/app/providers/connect-guard/connect-guard.tsx
+++ src/app/providers/connect-guard/connect-guard.tsx
@@ -1,27 +1,35 @@
 import { PropsWithChildren } from "react";
 import { useWalletButtonStatus } from "widgets/wallet-button";
-import { SnapContext, useGetSnapQuery } from "shared/snap";
+import { SnapContext, useGetSnapQuery, useIsFlaskQuery } from "shared/snap";
 import { SnapStep } from "./snap-step";
 import { WalletStep } from "./wallet-step";
 
 export const ConnectGuard = ({ children }: PropsWithChildren) => {
   const walletStatus = useWalletButtonStatus();
-
   const snapQuery = useGetSnapQuery();
+  const isFlaskQuery = useIsFlaskQuery();
+
+  console.log(isFlaskQuery.data);
 
   return (
     <>
-      {(walletStatus === "connectNeeded" ||
-        walletStatus === "switchNeeded") && <WalletStep />}
+      {isFlaskQuery.isSuccess && isFlaskQuery.data ? (
+        <>
+          {(walletStatus === "connectNeeded" ||
+            walletStatus === "switchNeeded") && <WalletStep />}
 
-      {walletStatus === "canDisconnect" && snapQuery.data === null && (
-        <SnapStep />
-      )}
+          {walletStatus === "canDisconnect" && snapQuery.data === null && (
+            <SnapStep />
+          )}
 
-      {snapQuery.data && walletStatus === "canDisconnect" && (
-        <SnapContext.Provider value={snapQuery.data}>
-          {children}
-        </SnapContext.Provider>
+          {snapQuery.data && walletStatus === "canDisconnect" && (
+            <SnapContext.Provider value={snapQuery.data}>
+              {children}
+            </SnapContext.Provider>
+          )}
+        </>
+      ) : (
+        <div>install metamask Flask</div>
       )}
     </>
   );
