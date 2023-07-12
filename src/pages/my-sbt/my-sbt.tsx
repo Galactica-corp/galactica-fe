b38629c68bd24733965c03103e7ad66a85376862
Aleksandr Abanin <aa@occam.fi>
2023-07-12 20:21:45 +0100

    change view

--- src/pages/my-sbt/my-sbt.tsx
+++ src/pages/my-sbt/my-sbt.tsx
@@ -28,28 +28,26 @@ export const MySBT = () => {
   );
 
   return (
-    <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center">
-      <div className={classNames("grid grid-cols-2 gap-[1rem] pb-8")}>
-        {zkCerts?.length === 0 && <ChooseKycProviderCard />}
-        {!hasBasicProof && zkCerts?.length !== 0 && <GenerateSbtCard />}
-        {query.data?.slice(0, 1).map((sbt, idx) => {
-          return (
-            <SbtCard
-              title={
-                sbt.dApp === CONTRACTS_ADDRESSES.REPEATABLE_ZK_KYC_TEST
-                  ? "KYC Proof"
-                  : "KYC Proof (Age > 18)"
-              }
-              key={idx}
-              provider="Example"
-              expiration={Date.now() + sbt.expirationTime}
-              level={1}
-            />
-          );
-        })}
+    <div className={classNames("grid grid-cols-3 gap-[1rem] pb-8")}>
+      {zkCerts?.length === 0 && <ChooseKycProviderCard />}
+      {!hasBasicProof && zkCerts?.length !== 0 && <GenerateSbtCard />}
+      {query.data?.slice(0, 1).map((sbt, idx) => {
+        return (
+          <SbtCard
+            title={
+              sbt.dApp === CONTRACTS_ADDRESSES.REPEATABLE_ZK_KYC_TEST
+                ? "KYC Proof"
+                : "KYC Proof (Age > 18)"
+            }
+            key={idx}
+            provider="Example"
+            expiration={Date.now() + sbt.expirationTime}
+            level={1}
+          />
+        );
+      })}
 
-        <LearnSbtCard />
-      </div>
+      <LearnSbtCard />
     </div>
   );
 };
