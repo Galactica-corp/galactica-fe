be43babae41fc663b1605828a0f8eea8bb0d3b5b
Aleksandr Abanin <aa@occam.fi>
2023-07-31 12:10:24 +0100

    handling assertion failed

--- src/app/providers/rq/provider.tsx
+++ src/app/providers/rq/provider.tsx
@@ -1,12 +1,27 @@
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { toastError } from "shared/utils/toasts";
 
 const client = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
       retry: false,
+      onError: (error) => {
+        if (
+          error &&
+          typeof error === "object" &&
+          "message" in error &&
+          typeof error.message === "string"
+        ) {
+          if (error.message.includes("Assertion failed")) {
+            toastError(
+              "Assertion failed. Try to reinstall your snap with other Wi-Fi or VPN"
+            );
+          }
+        }
+      },
     },
   },
 });
