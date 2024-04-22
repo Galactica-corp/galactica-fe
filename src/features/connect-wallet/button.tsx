dd0b453827b2e51302e2c4103fedfd4374fc17d4
alex-abanin <aa@occam.fi>
2024-04-22 13:55:05 +0300

    swithChain

--- src/features/connect-wallet/button.tsx
+++ src/features/connect-wallet/button.tsx
@@ -1,4 +1,12 @@
-import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { ComponentProps } from "react";
+
+import {
+  useAccount,
+  useChainId,
+  useConnect,
+  useDisconnect,
+  useSwitchChain,
+} from "wagmi";
 
 import { default as LogoutIcon } from "shared/icons/logout.svg?react";
 import { default as MetamaskIcon } from "shared/icons/metamask.svg?react";
@@ -6,49 +14,72 @@ import { Button } from "shared/ui/button";
 import { Spinner } from "shared/ui/spinner";
 import { shortAddress } from "shared/web3/utils";
 
+import { default as StarSvg } from "./star.svg?react";
+
 export function ConnectWalletButton() {
-  const { address, isConnected, isDisconnected, isConnecting } = useAccount();
+  const {
+    address,
+    isDisconnected,
+    isConnecting,
+    chainId: currentChainId,
+    connector,
+  } = useAccount();
+  const chainId = useChainId();
+
+  const { switchChain } = useSwitchChain();
   const { disconnect } = useDisconnect();
   const { connect, connectors } = useConnect();
 
-  const onClick = () => {
-    if (isConnected) {
-      disconnect();
-    }
-    if (isDisconnected) {
-      const metamaskConnector = connectors.find(
-        (connector) => connector.name === "MetaMask"
-      );
-      const flaskConnector = connectors.find((connector) => {
-        return connector.name === "MetaMask Flask";
-      });
+  const handleConnect = () => {
+    const metamaskConnector = connectors.find(
+      (connector) => connector.name === "MetaMask"
+    );
+    const flaskConnector = connectors.find((connector) => {
+      return connector.name === "MetaMask Flask";
+    });
 
-      const connector = metamaskConnector ?? flaskConnector;
+    const connector = metamaskConnector ?? flaskConnector;
 
-      connector && connect({ connector });
-    }
+    connector && connect({ connector });
   };
 
+  const btnProps: ComponentProps<typeof Button> = {
+    className: "w-[18.75rem] space-x-[0.9rem]",
+    isLoading: isConnecting,
+    theme: "primaryTransparent",
+  };
+
+  if (isDisconnected || isConnecting) {
+    return (
+      <Button {...btnProps} onClick={handleConnect}>
+        {isConnecting ? (
+          <Spinner />
+        ) : (
+          <>
+            <MetamaskIcon className="relative top-[-0.15rem]" />
+            <span>Connect MetaMask</span>
+          </>
+        )}
+      </Button>
+    );
+  }
+
+  if (currentChainId !== chainId) {
+    return (
+      <Button
+        {...btnProps}
+        onClick={() => switchChain({ connector: connector, chainId })}
+      >
+        <StarSvg className="size-6" />
+        <span>Switch network</span>
+      </Button>
+    );
+  }
+
   return (
-    <Button
-      className="w-[18.75rem] space-x-[0.9rem]"
-      isLoading={isConnecting}
-      onClick={onClick}
-      theme="primaryTransparent"
-    >
-      {isDisconnected && (
-        <>
-          <MetamaskIcon className="relative top-[-0.15rem]" />
-          <span>Connect MetaMask</span>
-        </>
-      )}
-      {isConnecting && <Spinner />}
-      {isConnected && (
-        <>
-          <span className="mr-1.5">{shortAddress(address, 4, 4)}</span>
-          <LogoutIcon className="relative top-[-0.1rem]" />
-        </>
-      )}
+    <Button {...btnProps} onClick={() => disconnect()}>
+      <span className="mr-1.5">{shortAddress(address, 4, 4)}</span>
+      <LogoutIcon className="relative top-[-0.1rem]" />
     </Button>
   );
 }
--- src/shared/providers/connect-guard/connect-guard.tsx
+++ src/shared/providers/connect-guard/connect-guard.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 
-import { useAccount } from "wagmi";
+import { useAccount, useChainId } from "wagmi";
 
 import { useGetSnapQuery } from "shared/snap";
 import { Spinner } from "shared/ui/spinner";
@@ -9,11 +9,17 @@ import { SnapStep } from "./ui/snap-step";
 import { WalletStep } from "./ui/wallet-step";
 
 export const ConnectGuard = ({ children }: PropsWithChildren) => {
-  const { isDisconnected, isConnecting } = useAccount();
+  const {
+    isDisconnected,
+    isConnecting,
+    chainId: currentChainId,
+  } = useAccount();
+  const chainId = useChainId();
 
   const snapQuery = useGetSnapQuery();
 
-  if (isDisconnected || isConnecting) return <WalletStep />;
+  if (isDisconnected || isConnecting || chainId !== currentChainId)
+    return <WalletStep />;
 
   if (snapQuery.isLoading) return <Spinner />;
 
