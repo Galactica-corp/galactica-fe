2ddb320a3170258a519efd73ebb9656f3c44abd7
alex-abanin <aa@occam.fi>
2024-07-08 16:03:03 +0300

    update ls keys

--- src/shared/snap/const.ts
+++ src/shared/snap/const.ts
@@ -1,8 +1,8 @@
 export const SNAP_LS_KEYS = {
-  zkCerts: `zk-certs-list-v2`,
-  zkHashes: (address: string | undefined) => `zk-hashes-${address}-v2`,
+  zkCerts: `zk-certs-list-v0.7.3`,
+  zkHashes: (address: string | undefined) => `zk-hashes-${address}-v0.7.3`,
   sbtDetails: (address: string | undefined) =>
-    `${import.meta.env.VITE_CHAIN_ID}-sbt-details-${address}-v2`,
+    `${import.meta.env.VITE_CHAIN_ID}-sbt-details-${address}-v0.7.3`,
   latestBlockChecked: (address: string | undefined) =>
-    `${address}-${import.meta.env.VITE_CHAIN_ID}-latest-block-checked`,
+    `${address}-${import.meta.env.VITE_CHAIN_ID}-latest-block-checked-v0.7.3`,
 };
